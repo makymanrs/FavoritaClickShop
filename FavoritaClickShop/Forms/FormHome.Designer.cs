a9d0e7c4f6de32bff9b1437aa945a18e867e98e2
makymanrs <132008304+makymanrs@users.noreply.github.com>
2024-07-25 13:54:06 -0600

    Cambios en los forecolors de formularios

--- FavoritaClickShop/Forms/FormHome.Designer.cs
+++ FavoritaClickShop/Forms/FormHome.Designer.cs
@@ -328,6 +328,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1082, 567);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.label7);
--- FavoritaClickShop/Forms/FormProducto.Designer.cs
+++ FavoritaClickShop/Forms/FormProducto.Designer.cs
@@ -381,6 +381,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1041, 599);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel3);
