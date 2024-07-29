7014ff91df4af71b8fc6a657a4f364c86b41d04f
makymanrs <132008304+makymanrs@users.noreply.github.com>
2024-07-29 14:39:55 -0600

    Nueva info

--- FavoritaClickShop/Mysql/Ccredito.cs
+++ FavoritaClickShop/Mysql/Ccredito.cs
@@ -20,20 +20,20 @@ namespace FavoritaClickShop.Mysql
                 conexion = objetoConexion.establecerConexion();
 
                 string query = @"SELECT 
-                        cre.cre_cod AS 'Credito ID',
-                        cre.fac_cod AS 'Factura Codigo',
-                        CONCAT(c.cli_pnom, ' ', IFNULL(c.cli_snom, ''), ' ', c.cli_pape, ' ', IFNULL(c.cli_sape, '')) AS 'Nombre Cliente',
-                        cre.cre_monto as 'Monto a pagar',
-                        f.fac_fec AS 'Fecha Factura',
-                        cre.cre_fecpagado as 'Fecha Pagada',   
-                        cre.cre_pagado AS 'Pagado'
-                     FROM 
-                        credito cre
-                     JOIN 
-                        factura f ON cre.fac_cod = f.fac_cod
-                     JOIN 
-                        cliente c ON f.cli_id = c.cli_cod
-                     ORDER BY cre.cre_cod ASC"; // Orden ascendente para cre_cod
+                cre.cre_cod AS 'Credito ID',
+                cre.fac_cod AS 'Factura Codigo',
+                CONCAT(c.cli_pnom, ' ', IFNULL(c.cli_snom, ''), ' ', c.cli_pape, ' ', IFNULL(c.cli_sape, '')) AS 'Nombre Cliente',
+                cre.cre_monto as 'Monto a pagar',
+                f.fac_fec AS 'Fecha Factura',
+                cre.cre_fecpagado as 'Fecha Pagada',   
+                cre.cre_pagado AS 'Pagado'
+             FROM 
+                credito cre
+             JOIN 
+                factura f ON cre.fac_cod = f.fac_cod
+             LEFT JOIN 
+                cliente c ON f.cli_id = c.cli_cod
+             ORDER BY cre.cre_cod ASC"; // Orden ascendente para cre_cod
 
                 MySqlDataAdapter adapter = new MySqlDataAdapter(query, conexion);
                 DataTable dt = new DataTable();
